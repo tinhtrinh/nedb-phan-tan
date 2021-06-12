751235f17f32a7a6b94ee8693a3844a7b4eb72a8
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-06-12 16:02:10 +0700

    api xóa

--- global/src/app.js
+++ global/src/app.js
@@ -6,11 +6,28 @@ const app = express();
 
 axios.get('http://localhost:5000/')
   .then(response => {
-    console.log(response.data);
+    // console.log(response.data);
   })
   .catch(error => {
     console.log(error);
 });
+
+// Api lấy nhân viên có MaNV = 'NV007' và ở cửa tiệm có MaCT = 'CH003' và chuyển sang cửa tiệm có MaCT = 'HN005';
+const MaNV = 'NV007';
+const MaCT = 'CH003'
+axios.post('http://localhost:5000/laynhanvien?MaNV=' + MaNV + '&MaCT=' + MaCT)
+  .then(res => {
+    //Thêm vào cửa tiệm khác
+    if(res){
+      console.log('NV: ', res.data);
+            
+    }
+  })
+  .catch(err => {
+    console.log('Err: ', err);
+  });
+
+
  
 app.get('/', function (req, res) {
     res.send('global');
@@ -19,3 +36,4 @@ app.get('/', function (req, res) {
 app.listen(3000, () => {
     console.log('listening at 3000')
 });
+
--- local/src/app.js
+++ local/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const Database = require('nedb');
 
 const app = express();
@@ -11,22 +12,22 @@ db.NhanVien.loadDatabase();
 db.CuaTiem.loadDatabase();
 
 //Insert data NhanVien
-db.NhanVien.insert({MaNV: 'NV001', HoTen: 'Phan Thị Tuyết', GioiTinh: 'Nữ', NgayVL: '01/01/2015', BacHoc: 'Đại học'});
-db.NhanVien.insert({MaNV: 'NV002', HoTen: 'Phạm Minh Hùng', GioiTinh: 'Nam', NgayVL: '01/04/2015', BacHoc: 'Cao Đẳng'});
-db.NhanVien.insert({MaNV: 'NV003', HoTen: 'Phạm Hoành Trí', GioiTinh: 'Nam', NgayVL: '01/03/2015', BacHoc: 'Cao Đẳng'});
-db.NhanVien.insert({MaNV: 'NV004', HoTen: 'Bùi Chính Bình', GioiTinh: 'Nam', NgayVL: '01/02/2015', BacHoc: 'Cao Đẳng'});
-db.NhanVien.insert({MaNV: 'NV005', HoTen: 'Phan Quỳnh Giang', GioiTinh: 'Nữ', NgayVL: '01/03/2015', BacHoc: 'Cao Đẳng'});
-db.NhanVien.insert({MaNV: 'NV006', HoTen: 'Nguyễn Thị Ngọc', GioiTinh: 'Nữ', NgayVL: '01/04/2015', BacHoc: 'Trung Cấp'});
-db.NhanVien.insert({MaNV: 'NV007', HoTen: 'Ngụy Thanh Hằng', GioiTinh: 'Nữ', NgayVL: '01/05/2015', BacHoc: 'Trung Cấp'});
-db.NhanVien.insert({MaNV: 'NV008', HoTen: 'Nguyễn Văn Đang', GioiTinh: 'Nữ', NgayVL: '01/06/2015', BacHoc: 'Trung Cấp'});
-db.NhanVien.insert({MaNV: 'NV009', HoTen: 'Dỗ Anh Minh', GioiTinh: 'Nam', NgayVL: '01/10/2015', BacHoc: 'Phổ Thông'});
-db.NhanVien.insert({MaNV: 'NV0010', HoTen: 'Đỗ Trung Quân', GioiTinh: 'Nam', NgayVL: '01/12/2015', BacHoc: 'Phổ Thông'});
+db.NhanVien.insert({MaNV: 'NV001', HoTen: 'Phan Thị Tuyết', GioiTinh: 'Nữ', NgayVL: '01/01/2015', BacHoc: 'Đại học', MaCT: 'CH001'});
+db.NhanVien.insert({MaNV: 'NV002', HoTen: 'Phạm Minh Hùng', GioiTinh: 'Nam', NgayVL: '01/04/2015', BacHoc: 'Cao Đẳng', MaCT: 'CH002'});
+db.NhanVien.insert({MaNV: 'NV003', HoTen: 'Phạm Hoành Trí', GioiTinh: 'Nam', NgayVL: '01/03/2015', BacHoc: 'Cao Đẳng', MaCT: 'CH003'});
+db.NhanVien.insert({MaNV: 'NV004', HoTen: 'Bùi Chính Bình', GioiTinh: 'Nam', NgayVL: '01/02/2015', BacHoc: 'Cao Đẳng', MaCT: 'CH004'});
+db.NhanVien.insert({MaNV: 'NV005', HoTen: 'Phan Quỳnh Giang', GioiTinh: 'Nữ', NgayVL: '01/03/2015', BacHoc: 'Cao Đẳng', MaCT: 'CH001'});
+db.NhanVien.insert({MaNV: 'NV006', HoTen: 'Nguyễn Thị Ngọc', GioiTinh: 'Nữ', NgayVL: '01/04/2015', BacHoc: 'Trung Cấp', MaCT: 'CH002'});
+db.NhanVien.insert({MaNV: 'NV007', HoTen: 'Ngụy Thanh Hằng', GioiTinh: 'Nữ', NgayVL: '01/05/2015', BacHoc: 'Trung Cấp', MaCT: 'CH003'});
+db.NhanVien.insert({MaNV: 'NV008', HoTen: 'Nguyễn Văn Đang', GioiTinh: 'Nữ', NgayVL: '01/06/2015', BacHoc: 'Trung Cấp', MaCT: 'CH004'});
+db.NhanVien.insert({MaNV: 'NV009', HoTen: 'Dỗ Anh Minh', GioiTinh: 'Nam', NgayVL: '01/10/2015', BacHoc: 'Phổ Thông', MaCT: 'CH001'});
+db.NhanVien.insert({MaNV: 'NV0010', HoTen: 'Đỗ Trung Quân', GioiTinh: 'Nam', NgayVL: '01/12/2015', BacHoc: 'Phổ Thông', MaCT: 'CH002'});
 
 //Insert data CuaTiem
-db.CuaTiem.insert({MaNV: 'CH001', TenCuaHang: 'Nhạc cụ Thiện Thanh', DiaChi: 'Quận Binh Thạnh, TPHCM'});
-db.CuaTiem.insert({MaNV: 'CH002', TenCuaHang: 'Nhạc cụ Thanh Hải', DiaChi: 'Quận Hải Châu, Đà Nẵng'});
-db.CuaTiem.insert({MaNV: 'CH003', TenCuaHang: 'Nhạc cụ Thiên Kiều', DiaChi: 'Quận Ninh Kiều, Cần Thơ'});
-db.CuaTiem.insert({MaNV: 'CH004', TenCuaHang: 'Nhạc cụ Thanh Lâm', DiaChi: 'Tp Đà Lạt, Lâm Đồng'});
+db.CuaTiem.insert({MaCT: 'CH001', TenCuaHang: 'Nhạc cụ Thiện Thanh', DiaChi: 'Quận Binh Thạnh, TPHCM'});
+db.CuaTiem.insert({MaCT: 'CH002', TenCuaHang: 'Nhạc cụ Thanh Hải', DiaChi: 'Quận 9, TPHCM'});
+db.CuaTiem.insert({MaCT: 'CH003', TenCuaHang: 'Nhạc cụ Thiên Kiều', DiaChi: 'Quận 12, TPHCM'});
+db.CuaTiem.insert({MaCT: 'CH004', TenCuaHang: 'Nhạc cụ Thanh Lâm', DiaChi: 'Thành phố Thủ Đức, TPHCM'});
  
 app.get('/', function (req, res) {
   db.CuaTiem.find({}, (err, data) => {
@@ -37,6 +38,26 @@ app.get('/', function (req, res) {
     res.json(data);
 });
 });
+
+
+
+app.post('/laynhanvien', (req, res) => {
+    db.NhanVien.find({ MaNV: req.query.MaNV, MaCT: req.query.MaCT }, (err, data) => {
+        if(err){
+            res.end();
+            return;
+        }
+        else if(data){
+            res.json(data);
+            db.NhanVien.remove({ _id: data[0]._id }, {multi: true}, (err, numRemoved) => {
+                console.log('Delete ' + numRemoved + ' rows');
+            });
+            
+        }
+    });
+
+});
+
  
 app.listen(5000, () => {
     console.log('listening at 5000')
